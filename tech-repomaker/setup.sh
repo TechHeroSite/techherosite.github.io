9e2e468f58ce407c981462204418d8de5315bc07
TechHeroSite <40578781+TechHeroSite@users.noreply.github.com>
2018-08-18 23:46:11 -0300

    Delete setup.sh

--- tech-repomaker/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-pip3 install -r requirements.txt --user
-mkdir -p data
-python3 manage.py makemigrations repomaker
-python3 manage.py migrate
-echo "All set up, now execute run.sh"
