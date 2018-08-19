deef333acc3516a68b40ccdf822959d0da570d5f
TechHeroSite <40578781+TechHeroSite@users.noreply.github.com>
2018-08-19 00:00:26 -0300

    Delete __init__.py

--- tech-repomaker/repomaker/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .apk import Apk
-from .apkpointer import ApkPointer, RemoteApkPointer
-from .app import App
-from .category import Category
-from .remoteapp import RemoteApp
-from .remoterepository import RemoteRepository
-from .repository import Repository
-from .screenshot import Screenshot, RemoteScreenshot
-from .storage import S3Storage, SshStorage, GitStorage
