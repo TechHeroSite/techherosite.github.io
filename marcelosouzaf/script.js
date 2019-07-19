52e333e5acc7d8ade36709eeb50746e5219a7b21
TechHeroSite <40578781+TechHeroSite@users.noreply.github.com>
2019-07-19 17:47:52 -0300

    TechHeroSite

--- marcelosouzaf/script.js
+++
@@ -1,8 +0,0 @@
-var pesquisa = "";
-
-function abrirUrl() {
-	pesquisa = document.getElementById("searchbox").value;
-	if (!(pesquisa == "")) {
-		window.open('https://www.youtube.com/results?search_query=marcelosouzaf+' + pesquisa);
-	}
-}
