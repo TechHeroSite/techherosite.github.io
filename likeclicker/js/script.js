5efac8222b6ba1888c8dab68975d2d86e0b3b0ad
TechHeroSite <40578781+TechHeroSite@users.noreply.github.com>
2019-08-02 20:11:30 -0300

    Like Clicker

--- likeclicker/js/script.js
+++ likeclicker/js/script.js
@@ -1670,3 +1670,22 @@ if (musicsEnabled == 1) {
 }
 
 loadGame();
+
+var btnAdd = document.getElementById("configoption16");
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  /*btnAdd.style.display = 'none';*/
+  document.querySelector('.InstallPWACategory').style.display = "none";
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
